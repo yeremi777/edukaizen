0fbbed99c953b1359bfa8e123850dd6aa6e9b28f
Yeremi Andrew <kurikarakenjou@gmail.com>
2025-01-05 12:56:06 +0700

    fix

--- src/main/java/com/kokuu/edukaizen/EdukaizenApplication.java
+++ src/main/java/com/kokuu/edukaizen/EdukaizenApplication.java
@@ -1,4 +1,4 @@
-package com.kokuu.edukaizen.edukaizen;
+package com.kokuu.edukaizen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;

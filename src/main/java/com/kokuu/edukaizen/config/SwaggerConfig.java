63cbf1514ee8c402842b3a4a03d8e5300a8e9a52
Yeremi Andrew <kurikarakenjou@gmail.com>
2025-01-13 12:18:16 +0700

    chore: set swagger info

---
+++ src/main/java/com/kokuu/edukaizen/config/SwaggerConfig.java
@@ -0,0 +1,18 @@
+package com.kokuu.edukaizen.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI custom() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("EduKaizen Documentation")
+                        .version("1.0.0"));
+    }
+}
